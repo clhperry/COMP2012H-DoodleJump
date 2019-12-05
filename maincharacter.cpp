036a2d22f348af67c51ad2f8bdfbac9d76d26db7
clhperry <53701943+clhperry@users.noreply.github.com>
2019-12-05 15:25:14 +0800

    Update maincharacter.cpp

--- maincharacter.cpp
+++ maincharacter.cpp
@@ -3,3 +3,14 @@
 Maincharacter::Maincharacter() {
   setImage(/*include image path later*/);
 }
+
+//move left & right
+Maincharacter::moveHorizontally() {
+  if (Keyboard::isKeyPressed(Keyboard::Right) && QKeyEvent::count() < 1) x+=3; //pseudocode - use QKeyEvent to implement
+  if (Keyboard::isKeyPressed(Keyboard::Left) && QKeyEvent::count() < 1) x-=3;
+}
+
+//Jump
+Maincharacter::moveVertically() {
+  if (Keyboard::isKeyPressed(Keyboard::Up) && QKeyEvent::count() < 1) y+=3; //pseudocode - use QKeyEvent to implement
+}

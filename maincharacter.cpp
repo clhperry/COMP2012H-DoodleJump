f9228e1e6bfc1e2d17ea7a1d56499f1dd3a99da1
clhperry <53701943+clhperry@users.noreply.github.com>
2019-12-10 13:03:09 +0800

    Update maincharacter.cpp

--- maincharacter.cpp
+++ maincharacter.cpp
@@ -1,13 +1,17 @@
 #include "maincharacter.h"
+//#include <QDebug>
 
 Maincharacter::Maincharacter() {
   setImage(/*include image path later*/);
 }
 
 //move left & right
-Maincharacter::moveHorizontally() {
-  if (Keyboard::isKeyPressed(Keyboard::Right) && QKeyEvent::count() < 1) x+=3; //pseudocode - use QKeyEvent to implement
-  if (Keyboard::isKeyPressed(Keyboard::Left) && QKeyEvent::count() < 1) x-=3;
+Maincharacter::moveHorizontally(QKeyEvent* event) {
+  if (event->key() == Qt::Key_Left && QKeyEvent::count() < 1) {
+    setPos(x()-10, y());
+  }//move left
+  if (event->key() == Qt::Key_Right && QKeyEvent::count() < 1) {
+    setPos(x()+10, y()); //move right
 }
 
 //Jump

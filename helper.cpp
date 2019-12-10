e35a022e67c11163ac660c4623bd3a5d02283f78
Eric Leung <Ehrick@users.noreply.github.com>
2019-12-10 13:26:15 +0800

    Update helper.cpp

--- helper.cpp
+++ helper.cpp
@@ -4,7 +4,7 @@ bool helper::checkCollision(Block& b1, Block& b2) {
   return b1.getRect().intersects(b2.getRect()); // intersected = collided = true
 }
 
-std::string Helper::getImagePath(std::string name, int status){
+std::string helper::getImagePath(std::string name, int status){
   if (name == "Maincharacter"){
     if (status == 0){
       return ":/resources/image/Maincharacter/" + std::to_string(status) + ".png";

15aa413f8a11819bf3868236e18893c44e9dd900
ImCombo <andyluk00@gmail.com>
2019-12-10 13:32:32 +0800

    Update helper.cpp

--- helper.cpp
+++ helper.cpp
@@ -1,10 +1,10 @@
 #include "helper.h"
 
-bool helper::checkCollision(Block& b1, Block& b2) {
+bool Helper::checkCollision(Block& b1, Block& b2) {
   return b1.getRect().intersects(b2.getRect()); // intersected = collided = true
 }
 
-std::string helper::getImagePath(std::string name, int status){
+std::string Helper::getImagePath(std::string name, int status){
   if (name == "Maincharacter"){
     if (status == 0){
       return ":/resources/image/Maincharacter/" + std::to_string(status) + ".png";

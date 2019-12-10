c76ae55893633cbbe4901a65238c401faf0b86c3
clhperry <53701943+clhperry@users.noreply.github.com>
2019-12-10 13:04:07 +0800

    Update maincharacter.cpp

--- maincharacter.cpp
+++ maincharacter.cpp
@@ -8,8 +8,8 @@ Maincharacter::Maincharacter() {
 //move left & right
 Maincharacter::moveHorizontally(QKeyEvent* event) {
   if (event->key() == Qt::Key_Left && QKeyEvent::count() < 1) {
-    setPos(x()-10, y());
-  }//move left
+    setPos(x()-10, y()); //move left
+  }
   if (event->key() == Qt::Key_Right && QKeyEvent::count() < 1) {
     setPos(x()+10, y()); //move right
 }

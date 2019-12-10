780eb84f877cab9b254043a6611961bceb3f27e2
Eric Leung <Ehrick@users.noreply.github.com>
2019-12-10 13:49:48 +0800

    Update helper.cpp

--- helper.cpp
+++ helper.cpp
@@ -1,19 +1,22 @@
 #include "helper.h"
 
-bool Helper::checkCollision(Block& b1, Block& b2) {
-  return b1.getRect().intersects(b2.getRect()); // intersected = collided = true
-}
+namespace helper {
 
-std::string Helper::getImagePath(std::string name, int status){
-  if (name == "Maincharacter"){
-    if (status == 0){
-      return ":/resources/image/Maincharacter/" + std::to_string(status) + ".png";
+    bool checkCollision(Block& b1, Block& b2) {
+      return b1.getRect().intersects(b2.getRect()); // intersected = collided = true
     }
-  }
-  if (name == "Platform"){
-    if (status >= 0 && status <= 2){
-      return ":/resources/image/Platform/" + std::to_string(status) + ".png";
-    }
-  }
-}
 
+    std::string getImagePath(std::string name, int status){
+      if (name == "Maincharacter"){
+        if (status == 0){
+          return ":/resources/image/Maincharacter/" + std::to_string(status) + ".png";
+        }
+      }
+      if (name == "Platform"){
+        if (status >= 0 && status <= 2){
+          return ":/resources/image/Platform/" + std::to_string(status) + ".png";
+        }
+      }
+    }
+
+}

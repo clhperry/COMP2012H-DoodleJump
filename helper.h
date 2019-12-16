c482713275b0f62de98cbd44a1b0ec9e6b319cf9
Eric Leung <Ehrick@users.noreply.github.com>
2019-12-16 18:22:30 +0800

    Update helper.h

--- helper.h
+++ helper.h
@@ -2,9 +2,12 @@
 #define HELPER_H
 
 #include "block.h"
+#include <vector>
+
 
 namespace helper {
-  std::string getImagePath(std::string name, int status = 0);
+  std::string getImagePath(std::string type, int status = 0);
+  std::string generateName(std::vector<Block> pool, std::string type);
   bool checkCollision(Block& b1, Block& b2);
 }
 

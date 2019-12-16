1f56a1a32a1cc4ec469400d5cc54229ef4dcde38
Eric Leung <Ehrick@users.noreply.github.com>
2019-12-16 18:22:08 +0800

    Update helper.cpp

--- helper.cpp
+++ helper.cpp
@@ -6,13 +6,17 @@ namespace helper {
       return b1.getRect().intersects(b2.getRect()); // intersected = collided = true
     }
 
-    std::string getImagePath(std::string name, int status){
-      if (name == "Maincharacter"){
+    std::string generateName(std::vector<Block> pool, std::string type) {
+        return type + std::to_string(pool.size());
+    }
+
+    std::string getImagePath(std::string type, int status){
+      if (type == "Maincharacter"){
         if (status == 0){
           return ":/resources/image/Maincharacter/" + std::to_string(status) + ".png";
         }
       }
-      if (name == "Platform"){
+      if (type == "Platform"){
         if (status >= 0 && status <= 2){
           return ":/resources/image/Platform/" + std::to_string(status) + ".png";
         }
